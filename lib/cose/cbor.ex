acf19e73313e9ee5eb344020d28387d85d4bc27c
Ryo Ito <ritou.06@gmail.com>
2018-01-25 10:33:33 +0900

    add functions related CBOR Tags (#4)

--- lib/cose/cbor.ex
+++ lib/cose/cbor.ex
@@ -3,12 +3,33 @@ defmodule COSE.CBOR do
   Erlang CBOR Wrapper module
   """
 
-  def encode(value) do
-    :cbor.encode(value)
-    |> :erlang.list_to_binary()
-  end
+  @spec encode(value :: any) :: binary
+  def encode(value), do: :cbor.encode(value) |> :erlang.list_to_binary()
+  @spec encode(cbor_binary :: binary) :: any
+  def decode(binary), do: :cbor.decode(binary)
 
-  def decode(binary) do
-    :cbor.decode(binary)
+  @cbor_tag_key %{
+    :CWT_CBOR => 61,
+    :COSE_Sign1 => 18,
+    :COSE_Mac0  => 17, 
+  }
+  @cbor_tag_value @cbor_tag_key |> Enum.into(%{}, fn {k, v} -> {v, k} end)
+
+  @spec tag(cwt :: any, tag :: atom) :: tuple | nil
+  def tag(object, tag) when is_atom(tag) do
+    case @cbor_tag_key[tag] do
+      nil -> nil
+      tag_value -> {:tag, tag_value, object}
+    end
   end
+  def tag(_, _), do: nil
+  
+  @spec parse_tag(tagged_cwt :: tuple) :: {tag_key :: atom, object :: any} | nil
+  def parse_tag({:tag, tag_value, object}) when is_integer(tag_value) do
+    case @cbor_tag_value[tag_value] do
+      nil -> nil
+      tag_key -> {tag_key, object}
+    end
+  end
+  def parse_tag(_), do: nil
 end
