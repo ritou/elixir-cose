2310211af26fd8dbcd60d118033074720d97918d
ritou <ritou.06@gmail.com>
2018-02-25 01:29:24 +0900

    add jose to deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Cose.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :jose]
     ]
   end
 
@@ -23,6 +23,7 @@ defmodule Cose.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:jose, "~> 1.8"},
 
       # Erlang CBOR library
       {:cbor, github: "yjh0502/cbor-erlang"}
